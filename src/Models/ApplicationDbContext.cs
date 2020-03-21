ad7fc9dc32a4c28c0961bbc93da293e5aaaf6194
SaschaMendel <saschamendel2016@gmail.com>
2020-03-21 17:34:49 +0100

    BookingAccessoires

--- src/Models/ApplicationDbContext.cs
+++ src/Models/ApplicationDbContext.cs
@@ -19,10 +19,12 @@ namespace Models
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<BookingBeestje>().HasKey(sc => new { sc.BookingId, sc.BeestjeId});
+            modelBuilder.Entity<BookingAccessoires>().HasKey(sc => new { sc.BookingId, sc.AccessoireId });
+
         }
 
         public DbSet<BookingBeestje> BookingBeestjes { get; set; }
-
+        public DbSet<BookingAccessoires> BookingAccessoires { get; set; }
         public DbSet<Accessoire> Accessoires { get; set; }
         public DbSet<Booking> Bookings { get; set; }
         public DbSet<Beestje> Beestjes { get; set; }
---
+++ src/Web/Migrations/20200321163358_AddBookingAccessoires.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web.Migrations
+{
+    public partial class AddBookingAccessoires : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BookingAccessoires",
+                columns: table => new
+                {
+                    BookingId = table.Column<int>(nullable: false),
+                    AccessoireId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookingAccessoires", x => new { x.BookingId, x.AccessoireId });
+                    table.ForeignKey(
+                        name: "FK_BookingAccessoires_Accessoires_AccessoireId",
+                        column: x => x.AccessoireId,
+                        principalTable: "Accessoires",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookingAccessoires_Bookings_BookingId",
+                        column: x => x.BookingId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookingAccessoires_AccessoireId",
+                table: "BookingAccessoires",
+                column: "AccessoireId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookingAccessoires");
+        }
+    }
+}
