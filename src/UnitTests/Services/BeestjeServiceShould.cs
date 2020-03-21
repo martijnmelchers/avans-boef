8c56e6ace531e4a69470144885bfa0830a675db1
SaschaMendel <saschamendel2016@gmail.com>
2020-03-21 13:32:30 +0100

    removed crap

---
+++ src/UnitTests/Services/BeestjeServiceShould.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using DomainServices;
+using DomainServices.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Models.Repository;
+using System.Collections.Generic;
+using Models;
+using Models.Repository.Interfaces;
+using Moq;
+
+namespace UnitTests.Services
+{
+    class BeestjeServiceShould
+    {
+        private readonly IBeestjeService _beestjeService;
+
+        public BeestjeServiceShould()
+        {
+        }
+    }
+}
